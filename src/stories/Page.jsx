4b76aee4819be1d04369340944cee4004a33a197
Infamou5 <oreal_b52@hotmail.com>
2022-01-18 23:31:14 +0100

    Added and tested Input Fields @ storybook Page

--- src/stories/Page.jsx
+++ src/stories/Page.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Header } from './Header';
+import {LastNameInputField }from "../components/InputField/InputField.stories"
+import {FirstNameInputField } from "../components/InputField/InputField.stories"
 import './page.css';
 
 export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
+  <div>
   <article>
     <Header user={user} onLogin={onLogin} onLogout={onLogout} onCreateAccount={onCreateAccount} />
-
     <section>
       <h2>{user ? "Log Out" : "Log In"}</h2>
       </section>
-  </article>
+   </article>
+     <FirstNameInputField label={"First name:"} isMode="default"/>
+     <LastNameInputField label={"Last name:"} isMode="default" />
+  </div>
 );
 Page.propTypes = {
   user: PropTypes.shape({}),
